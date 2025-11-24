e1f7105ef89427ec68dee085f3cd469e67601ea9
Israel Antonio <israelantonio027@gmail.com>
2025-11-24 16:43:05 -0300

    atualização da regra de criação do atributo perfil, criação de um construtor  e métodos da entidade
    Criação dos métodos para atualizar dados e desativar usuário.

--- src/main/java/com/SmartBooking/Modelos/Usuario/Usuario.java
+++ src/main/java/com/SmartBooking/Modelos/Usuario/Usuario.java
@@ -2,8 +2,11 @@ package com.SmartBooking.Modelos.Usuario;
 
 import com.SmartBooking.Modelos.Reserva.Reserva;
 import jakarta.persistence.*;
+import jakarta.validation.Valid;
 import lombok.*;
 import org.hibernate.annotations.Cascade;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -25,11 +28,34 @@ public class Usuario {
     private String email;
     private String senha;
     @Enumerated(EnumType.STRING)
-    private Perfil perfil;
+    private Perfil perfil = Perfil.USUARIO;
+    private Boolean ativo = true;
+
 
     @OneToMany(mappedBy = "usuario", cascade = CascadeType.ALL, fetch = FetchType.EAGER)
     private List<Reserva> reservas = new ArrayList<>();
 
 
+    public Usuario(@Valid DadosCriacaoUsuario dados) {
+        this.nome = dados.nome();
+        this.email = dados.email();
+        this.senha = dados.senha();
+
+    }
+
+    public void atulizarDados(@Valid DadosAtualizacaoDoUsuario dados) {
+        if(dados.nome() != null){
+            this.nome = dados.nome();
+        }
+        if(dados.email() != null){
+           this.email = dados.email();
+        }
+        if(dados.senha() != null){
+            this.senha = dados.senha();
+        }
+    }
+    public void excluir(){
+        this.ativo = false;
+    }
 
 }
