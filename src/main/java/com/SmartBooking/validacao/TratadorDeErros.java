74e756c281e9c5b2ae9e2652f03252908fdd627d
Israel Antonio <israelantonio027@gmail.com>
2025-11-30 17:16:06 -0300

    Criei dois tipos de classe para tratamento de erro em casos mais específicos do projeto e que possam ir contra as regras de negócio.

---
+++ src/main/java/com/SmartBooking/validacao/TratadorDeErros.java
@@ -0,0 +1,14 @@
+package com.SmartBooking.validacao;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+@RestControllerAdvice
+public class TratadorDeErros {
+
+    @ExceptionHandler(ValidacaoException.class)
+    public ResponseEntity<?> tratarErro(ValidacaoException ex) {
+        return ResponseEntity.badRequest().body(ex.getMessage());
+    }
+}
