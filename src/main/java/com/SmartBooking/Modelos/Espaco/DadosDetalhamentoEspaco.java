13a7e04189317ca905e25364f77ffce4eb785637
Israel Antonio <israelantonio027@gmail.com>
2025-12-26 17:17:09 -0300

    Adição de dois novos valores no DTO de detalhamento de espaço .
    Na atualização foram adicionados os valores de abertura e fechamento do espaço inclusive em seu construtor.

--- src/main/java/com/SmartBooking/Modelos/Espaco/DadosDetalhamentoEspaco.java
+++ src/main/java/com/SmartBooking/Modelos/Espaco/DadosDetalhamentoEspaco.java
@@ -3,9 +3,11 @@ package com.SmartBooking.Modelos.Espaco;
 import jakarta.validation.Valid;
 import jakarta.validation.constraints.NotNull;
 
-public record DadosDetalhamentoEspaco(Long Id, String nome, String email,Integer capacidade,String tipodeAmbiente,Long endereco,String andar,String referencia ) {
+import java.time.LocalTime;
+
+public record DadosDetalhamentoEspaco(Long Id, String nome, String email, Integer capacidade, String tipodeAmbiente, Long endereco, String andar, String referencia, LocalTime abertura,  LocalTime fechamento) {
    public DadosDetalhamentoEspaco(Espaco dados){
-       this(dados.getId(), dados.getNome(), dados.getEmail(), dados.getCapacidade(), dados.getAmbiente(), dados.getEndereco().getId(), dados.getAndar(), dados.getReferencia());
+       this(dados.getId(), dados.getNome(), dados.getEmail(), dados.getCapacidade(), dados.getAmbiente(), dados.getEndereco().getId(), dados.getAndar(), dados.getReferencia(), dados.getAbertura(), dados.getFechamento());
    }
 
 }
