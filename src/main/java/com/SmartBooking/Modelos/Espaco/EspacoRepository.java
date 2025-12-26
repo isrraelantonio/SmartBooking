c0000fec4d568a79addb22630dfee337d978f313
Israel Antonio <israelantonio027@gmail.com>
2025-12-26 17:11:29 -0300

    Criação da pesquisa por espaços ativados e desativados no banco de dados.
    Nas pesquisas foi levado em consideração o status do espaço.

--- src/main/java/com/SmartBooking/Modelos/Espaco/EspacoRepository.java
+++ src/main/java/com/SmartBooking/Modelos/Espaco/EspacoRepository.java
@@ -2,9 +2,15 @@ package com.SmartBooking.Modelos.Espaco;
 
 import jakarta.validation.constraints.NotNull;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 public interface EspacoRepository extends JpaRepository<Espaco, Long> {
     boolean existsByEmail(@NotNull String email);
 
+    @Query("SELECT e FROM Espaco e WHERE e.id = :id AND e.disponivel = true")
+    Espaco existeEspaco(@NotNull Long id);
+
+    @Query("SELECT e FROM Espaco e WHERE e.id = :id AND e.disponivel = false")
+    Espaco existeEspacoDesativado(@NotNull Long id);
 
 }
