d21419f3397b5121abe85f7b587c703af7595057
Israel Antonio <israelantonio027@gmail.com>
2025-12-26 17:14:22 -0300

    Atualização do modelo Espaço
    Na atualização do objeto foram adicionados os elementos de abertura e fechamento do espaço, além disso, os métodos de ativação e desativação do mesmo.

--- src/main/java/com/SmartBooking/Modelos/Espaco/Espaco.java
+++ src/main/java/com/SmartBooking/Modelos/Espaco/Espaco.java
@@ -5,6 +5,8 @@ import jakarta.persistence.*;
 import jakarta.validation.Valid;
 import lombok.*;
 
+import java.time.LocalTime;
+
 @Entity(name = "Espaco")
 @Table(name = "espacos")
 @Getter
@@ -25,6 +27,8 @@ public class Espaco {
     private Endereco endereco;
     private String andar;
     private String referencia;
+    private LocalTime abertura;
+    private LocalTime fechamento;
     private Boolean disponivel = true;
 
 
@@ -36,7 +40,22 @@ public class Espaco {
         this.endereco = endereco;
         this.andar = dados.andar();
         this.referencia = dados.referencia();
+        this.abertura = dados.abertura();
+        this.fechamento = dados.fechamento();
+
+
 
     }
+
+    public void desativarespaco(){
+        this.disponivel = false;
+
+    }
+
+    public void ativarespaco(){
+        this.disponivel = true;
+
+    }
+
 
 }
