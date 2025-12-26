7f63cf98892dbe9d0cac7ce91b6027819af2b929
Israel Antonio <israelantonio027@gmail.com>
2025-12-26 16:48:23 -0300

    Atualização dos dados de criação de reseva
    Nesse caso por se tratar da criação de reserva pelo usuário fiz a retirada da inserção de status e adicionei a validação para a verificação da inserção do horário de fim.

--- src/main/java/com/SmartBooking/Modelos/Reserva/DadosCriarReserva.java
+++ src/main/java/com/SmartBooking/Modelos/Reserva/DadosCriarReserva.java
@@ -1,12 +1,17 @@
 package com.SmartBooking.Modelos.Reserva;
 
+import jakarta.validation.constraints.NotNull;
+
 import java.time.LocalDateTime;
 
 public record DadosCriarReserva(
+        @NotNull
         Long usuarioId,
+        @NotNull
         Long espacoId,
+        @NotNull
         LocalDateTime inicio,
-        LocalDateTime fim,
-        StatusConfirmacao status
+        @NotNull
+        LocalDateTime fim
 ) {
 }
