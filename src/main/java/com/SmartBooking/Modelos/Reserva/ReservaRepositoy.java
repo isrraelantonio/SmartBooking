cc174b6cff420c28956a94a07194f2f5f71d7f91
Israel Antonio <israelantonio027@gmail.com>
2025-12-26 17:04:01 -0300

    Criação de repositório de reservas.

--- src/main/java/com/SmartBooking/Modelos/Reserva/ReservaRepositoy.java
+++ src/main/java/com/SmartBooking/Modelos/Reserva/ReservaRepositoy.java
@@ -1,6 +1,9 @@
 package com.SmartBooking.Modelos.Reserva;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 public interface ReservaRepositoy extends JpaRepository<Reserva, Long> {
+
+
 }
