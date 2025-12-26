1ad029a826650fc7360c100649d0b76f0a97a0b7
Israel Antonio <israelantonio027@gmail.com>
2025-12-26 17:21:32 -0300

    No DTO de  atualização de espaço foi adicionado os  valores de abertura e de fechamento do espaço.

--- src/main/java/com/SmartBooking/Modelos/Espaco/AtualizarEspaco.java
+++ src/main/java/com/SmartBooking/Modelos/Espaco/AtualizarEspaco.java
@@ -4,15 +4,18 @@ import com.SmartBooking.Modelos.Endereco.Endereco;
 import jakarta.validation.Valid;
 import jakarta.validation.constraints.NotNull;
 
+import java.time.LocalTime;
+
 public record AtualizarEspaco(
-        @NotNull
-        Long id,
         String nome,
         String email,
         Integer capacidade,
         String tipodeAmbiente,
         Endereco endereco,
         String andar,
-        String referencia
+        String referencia,
+        LocalTime abertura,
+        LocalTime fechamento
 ) {
+
 }
