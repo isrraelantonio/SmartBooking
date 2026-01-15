1ce55cc1510b39acb5f518cae8ce83d60929140d
Israel Antonio <israelantonio027@gmail.com>
2026-01-15 16:38:24 -0300

    Criação da query de busca por reservas.

--- src/main/java/com/SmartBooking/Modelos/Reserva/ReservaRepositoy.java
+++ src/main/java/com/SmartBooking/Modelos/Reserva/ReservaRepositoy.java
@@ -1,9 +1,28 @@
 package com.SmartBooking.Modelos.Reserva;
 
+import jakarta.validation.constraints.NotNull;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import java.time.LocalDateTime;
+import java.util.List;
+import java.util.Optional;
 
 public interface ReservaRepositoy extends JpaRepository<Reserva, Long> {
 
+    @Query("""
+    SELECT r FROM Reserva r
+    WHERE r.id = :id
+    AND r.status IN :status
+""")
+    Reserva existeReservaDesativadaNegada(
+            @Param("id") Long id,
+            @Param("status") List<StatusReserva> status
+    );
 
+
+    List<Reserva> findByStatusAndFimBefore(StatusReserva statusReserva, LocalDateTime now);
 }
+
+
