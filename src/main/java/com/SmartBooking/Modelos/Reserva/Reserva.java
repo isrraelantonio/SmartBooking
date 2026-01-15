4c8978e6e88eab23dab2244635eb14bb6640817a
Israel Antonio <israelantonio027@gmail.com>
2026-01-15 16:37:20 -0300

    mudança no valor do status da reserva e criação de alguns métodos relacionados a mesma.
    Métodos criados : de atualização de reserva pelo administrador do sistema e de usuário.

--- src/main/java/com/SmartBooking/Modelos/Reserva/Reserva.java
+++ src/main/java/com/SmartBooking/Modelos/Reserva/Reserva.java
@@ -1,6 +1,9 @@
 package com.SmartBooking.Modelos.Reserva;
 
 import com.SmartBooking.Modelos.Espaco.Espaco;
+import com.SmartBooking.Modelos.Reserva.dto.AtualizacaoReservaAdmDTO;
+import com.SmartBooking.Modelos.Reserva.dto.AtualizacaoReservaUsuarioDTO;
+import com.SmartBooking.Modelos.Reserva.dto.CriacaoReservaDTO;
 import com.SmartBooking.Modelos.Usuario.Usuario;
 import jakarta.persistence.*;
 import lombok.*;
@@ -32,16 +35,55 @@ public class Reserva {
     private LocalDateTime fim;
 
     @Enumerated(EnumType.STRING)
-    private StatusConfirmacao status = StatusConfirmacao.PENDENTE;
+    private StatusReserva status = StatusReserva.PENDENTE;
 
     private String observacao = "";
 
 
-    public Reserva(DadosCriarReserva dados, Usuario usuario, Espaco espaco) {
+    public Reserva(CriacaoReservaDTO dados, Usuario usuario, Espaco espaco) {
         this.usuario = usuario;
         this.espaco = espaco;
         this.inicio = dados.inicio();
         this.fim = dados.fim();
+    }
+
+    public void atualizarReservaAdm(AtualizacaoReservaAdmDTO dados, Espaco espaco, Usuario usuario){
+        if(dados.inicio() != null){
+            this.inicio = dados.inicio();
+        }if(dados.fim() != null){
+            this.fim = dados.fim();
+        }if(dados.status() != null){
+            this.status = StatusReserva.fromString(dados.status());
+        }if(dados.observacao() != null){
+            this.observacao = dados.observacao();
+        }
+        if (espaco != null && !espaco.equals(getEspaco())) {
+            this.espaco = espaco;
+        } // Aqui tenho uma estrutura de decisão com sobrescrição de equals e hashcode dentro do objeto Espaço.
+
+        if(usuario != null && !usuario.getId().equals(getUsuario().getId())){
+            this.usuario = usuario; // Nesse caso usei uma verificação mais simples sem sobrescrições no usuário.
+        }
+
+    }
+
+    public void atualizarReservaUsuario(AtualizacaoReservaUsuarioDTO dados, Espaco espaco) {
+        if (dados.inicio() != null) {
+            this.inicio = dados.inicio();
+        }
+        if (dados.fim() != null) {
+            this.fim = dados.fim();
+        }
+        if (espaco != null && !espaco.equals(getEspaco())) {
+            this.espaco = espaco;
+        }
+
+    }
+
+    public void finalizar() {
+        if (this.status == StatusReserva.CONFIRMADO) {
+            this.status = StatusReserva.FINALIZADA;
+        }
     }
 
 }
