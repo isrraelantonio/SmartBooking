9f929b96b5c036961c1a957c34cec6c89779d605
Israel Antonio <israelantonio027@gmail.com>
2026-01-15 15:53:55 -0300

    mudança no diretório do tratador de erro

--- src/main/java/com/SmartBooking/exception/TratadorDeErros.java
+++ src/main/java/com/SmartBooking/exception/TratadorDeErros.java
@@ -1,4 +1,4 @@
-package com.SmartBooking.exceções;
+package com.SmartBooking.exception;
 
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
