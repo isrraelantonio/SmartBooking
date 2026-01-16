6d256ab30b4d6b0656f3ee5bd5b3ac0463afa7a8
Israel Antonio <israelantonio027@gmail.com>
2026-01-16 18:10:39 -0300

    feat: adiciona tratador global de erros 400 com DTO de resposta
    - Adiciona tratador global com @RestControllerAdvice
    - Centraliza tratamento de MethodArgumentNotValidException
    - Padroniza resposta retornando apenas campo e mensagem

--- src/main/java/com/SmartBooking/exception/TratadorDeErros.java
+++ src/main/java/com/SmartBooking/exception/TratadorDeErros.java
@@ -1,6 +1,8 @@
 package com.SmartBooking.exception;
 
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
@@ -10,5 +12,21 @@ public class TratadorDeErros {
     @ExceptionHandler(ValidacaoException.class)
     public ResponseEntity<?> tratarErro(ValidacaoException ex) {
         return ResponseEntity.badRequest().body(ex.getMessage());
+    }
+
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity tratarErro400(MethodArgumentNotValidException ex){
+        var erros = ex.getFieldErrors();
+
+        return ResponseEntity.badRequest().body(erros.stream()
+                .map(DadosErrorValidacao::new).toList());
+    }
+
+    private record DadosErrorValidacao(String campo, String mensagem){
+
+        private DadosErrorValidacao (FieldError erro){
+            this(erro.getField(), erro.getDefaultMessage());
+        }
     }
 }
