7f4297ce7980bb4195200eba8b143c3f49632510
Israel Antonio <israelantonio027@gmail.com>
2026-02-01 10:42:42 -0300

    feat: adiciona queries de listagem de reservas no repository.
    Cria duas novas consultas no repositório de reservas: uma responsável pela listagem paginada de todas as reservas e outra para a listagem filtrada por identificador do usuário.

--- src/main/java/com/SmartBooking/Modelos/Reserva/ReservaRepositoy.java
+++ src/main/java/com/SmartBooking/Modelos/Reserva/ReservaRepositoy.java
@@ -1,6 +1,8 @@
 package com.SmartBooking.Modelos.Reserva;
 
 import jakarta.validation.constraints.NotNull;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
@@ -21,6 +23,14 @@ public interface ReservaRepositoy extends JpaRepository<Reserva, Long> {
             @Param("status") List<StatusReserva> status
     );
 
+    @Query("""
+            SELECT r FROM Reserva r
+            WHERE r.usuario.id = :id
+            """)
+    Page<Reserva> reservasDoUsuario(Long id, Pageable paginacao);
+
+
+    Page<Reserva> findAll(Pageable paginacao);
 
     List<Reserva> findByStatusAndFimBefore(StatusReserva statusReserva, LocalDateTime now);
 }
