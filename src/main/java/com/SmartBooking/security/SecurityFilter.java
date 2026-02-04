d89dbb1d55e6f63c084a1cb67900a253fd150df9
Israel Antonio <israelantonio027@gmail.com>
2026-02-04 17:17:06 -0300

    feat(security): implementa filtro de segurança para autenticação.
    Implementa a classe SecurityFilter  com métodos para recuperação do token JWT e aplicação do filtro de autenticação, integrando o fluxo de segurança ao Spring Security.

---
+++ src/main/java/com/SmartBooking/security/SecurityFilter.java
@@ -0,0 +1,72 @@
+package com.SmartBooking.security;
+
+
+import com.SmartBooking.Modelos.Usuario.UsuarioRepository;
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.stereotype.Component;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import java.io.IOException;
+
+@Component
+public class SecurityFilter extends OncePerRequestFilter {
+
+    @Autowired
+    private TokenService tokenService;
+
+    @Autowired
+    private UsuarioRepository repository;
+
+    @Override
+    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
+
+
+        var tokenJWT = recuperarToken(request);
+
+
+        if (tokenJWT != null){
+            var subject = tokenService.getsubject(tokenJWT);
+            var usuario = repository.findBySenha(subject);
+
+            var authentication = new UsernamePasswordAuthenticationToken(usuario, null, usuario.getAuthorities());
+            SecurityContextHolder.getContext().setAuthentication(authentication);
+            System.out.println("LOGADO NA REQUISIÇÃO");
+        }
+
+        filterChain.doFilter(request, response);
+    }
+
+
+    private String recuperarToken(HttpServletRequest request){
+
+        var authorizationHeader = request.getHeader("Authorization");
+        if (authorizationHeader == null || authorizationHeader.isBlank()) {
+            return null;
+        }
+
+        if (!authorizationHeader.startsWith("Bearer ")) {
+            return null;
+        }
+
+        return authorizationHeader.substring(7);
+    }
+
+    /***
+     * versão alternativa que também pode ser usada como recuperador de token.
+     * O código acima é uma versão melhorada do mesmo.
+     */
+//    private String recuperarToken(HttpServletRequest request) {
+//        var autorizationHeader = request.getHeader("Authorization");
+//        if(autorizationHeader != null){
+//            return autorizationHeader.replace("Bearer ", "");
+//        }
+//        return  null;
+
+
+}
