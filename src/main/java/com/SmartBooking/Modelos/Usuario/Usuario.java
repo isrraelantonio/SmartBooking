3966800311160549cc47483b45a5eda4c725276e
Israel Antonio <israelantonio027@gmail.com>
2026-02-04 17:00:11 -0300

    feat(security): implementa UserDetails na entidade Usuario.
    Adiciona a implementação da interface UserDetails na entidade Usuario, expondo email como username, senha para autenticação e mapeando os
    perfis do usuário para roles do Spring Security.

--- src/main/java/com/SmartBooking/Modelos/Usuario/Usuario.java
+++ src/main/java/com/SmartBooking/Modelos/Usuario/Usuario.java
@@ -6,8 +6,12 @@ import com.SmartBooking.Modelos.Usuario.dto.DadosCriacaoUsuarioDTO;
 import jakarta.persistence.*;
 import jakarta.validation.Valid;
 import lombok.*;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
 
 import java.util.ArrayList;
+import java.util.Collection;
 import java.util.List;
 
 import static org.hibernate.engine.internal.Cascade.cascade;
@@ -19,7 +23,7 @@ import static org.hibernate.engine.internal.Cascade.cascade;
 @NoArgsConstructor
 @AllArgsConstructor
 @EqualsAndHashCode(of = "id")
-public class Usuario {
+public class Usuario  implements UserDetails {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
@@ -53,4 +57,30 @@ public class Usuario {
         this.ativo = false;
     }
 
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return List.of(new SimpleGrantedAuthority("ROLE_"+ perfil.name()));
+    }
+
+    @Override
+    public String getPassword() {
+        return this.senha;
+    }
+
+    @Override
+    public String getUsername() {
+        return this.email;
+    }
+
+    @Override
+    public boolean isAccountNonExpired() { return true;}
+
+    @Override
+    public boolean isAccountNonLocked() { return true;}
+
+    @Override
+    public boolean isCredentialsNonExpired() { return true;}
+
+    @Override
+    public boolean isEnabled() { return true;}
 }
