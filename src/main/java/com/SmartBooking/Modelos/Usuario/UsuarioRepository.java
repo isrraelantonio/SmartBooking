c35e8e0440e7c57964455981b4c4fb5898c07927
Israel Antonio <israelantonio027@gmail.com>
2026-02-04 17:10:20 -0300

    feat: adiciona recuperação de usuário para fluxo de autenticação
    feat: adiciona recuperação de usuário para fluxo de autenticação

--- src/main/java/com/SmartBooking/Modelos/Usuario/UsuarioRepository.java
+++ src/main/java/com/SmartBooking/Modelos/Usuario/UsuarioRepository.java
@@ -1,10 +1,15 @@
 package com.SmartBooking.Modelos.Usuario;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.security.core.userdetails.UserDetails;
 
 public interface UsuarioRepository extends JpaRepository<Usuario, Long> {
 
     boolean existsByEmail(String email);
 
     boolean existsByIdAndAtivoTrue(Long id);
+
+    UserDetails findByEmail(String login);
+
+    UserDetails findBySenha(String login);
 }
